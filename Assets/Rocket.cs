b7b68a4ba990152faf6c9cdd3404922db5f4b784
Larsto <huhtalaster@gmail.com>
2018-05-17 16:25:32 +0300

    testailua

---
+++ Assets/Rocket.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+    [SerializeField] float rocketRotation = 10;
+    [SerializeField] float rocketThrust = 10;
+
+    // Use this for initialization
+    void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Thrust();
+        Rotate();
+	}
+
+    private void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up* rocketThrust);
+            if (!audioSource.isPlaying)
+            
+                {
+                    audioSource.Play();
+
+                }
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+    }
+
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true; // take manual control of rotation
+        float rorationSpeed = rocketRotation * Time.deltaTime;
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.forward * rorationSpeed);
+        }
+
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            
+            transform.Rotate(-Vector3.forward * rorationSpeed);
+        }
+        rigidBody.freezeRotation = false; // resume physics control of rotation
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log("Collition");
+                break;
+            case "Fuel":
+                Debug.Log("Fuel!");
+                break;
+            default:
+                Debug.Log("Death!");
+                break;
+        }
+    }
+}
